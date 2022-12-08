63cf45d6a0c4af0e1b1534565ea3ae1053abfe34
omarfarukee <umarfaruque1920@gmail.com>
2022-12-08 14:22:42 +0600

    stokes products api done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000
 
 const app = express()
@@ -9,10 +9,38 @@ const app = express()
 app.use(cors())
 app.use(express.json()) 
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xdpsuxi.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 async function run() {
-
     try{
+            const productsCollection = client.db('startingCore').collection('categories')
+            const stokesProductsCollection = client.db('startingCore').collection('stocksProduct')
 
+        
+            app.get('/categories', async (req, res) => {
+                const query = {}
+                const result = await productsCollection.find(query).toArray()
+                res.send(result)
+            })
+
+        app.get('/stocksProduct', async (req, res) => {
+            const query = {}
+            const result = await stokesProductsCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        app.get('/stocksProduct/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { categoryId: id };
+            console.log(query)
+            const result = await stokesProductsCollection.find(query).toArray();
+            res.send(result);
+            
+        })
     }
     finally{
 
